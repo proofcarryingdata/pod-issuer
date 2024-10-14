19367229c8d57c4e59edf13ab24fa5889651641f
Ahmad <root@ahmadafuni.com>
2024-10-14 12:47:21 +1000

    Fix BSD sed compatibility

--- scripts/patch-ffjavascript.sh
+++ scripts/patch-ffjavascript.sh
@@ -5,9 +5,9 @@ threadman_file=node_modules/.deno/ffjavascript*/node_modules/ffjavascript/src/th
 
 
 # Comment out Worker-related definitions
-sed -i 's/^\(var Worker.*\)/\/\/\ \1/' $main_js_file
-sed -i 's/^import Worker.*//' $threadman_file
+sed -i.bak 's/^\(var Worker.*\)/\/\/\ \1/' $main_js_file
+sed -i.bak 's/^import Worker.*//' $threadman_file
 
 # Replace Worker calls
-sed -i 's/\(.*new Worker\).*[(]\(.*\)[)].*/\1(\2, { type: "module" });/' $main_js_file
-sed -i 's/\(.*new Worker\).*[(]\(.*\)[)].*/\1(\2, { type: "module" });/g' $threadman_file
+sed -i.bak 's/\(.*new Worker\).*[(]\(.*\)[)].*/\1(\2, { type: "module" });/' $main_js_file
+sed -i.bak 's/\(.*new Worker\).*[(]\(.*\)[)].*/\1(\2, { type: "module" });/g' $threadman_file
